0ebe0afe74bc1a7c37da2a8b8f8ef6b026f8ffd0
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-07 02:44:08 -0300

    Update CalculatorCircularBasket.tsx

--- src/pages/CalculatorCircularBasket/CalculatorCircularBasket.tsx
+++ src/pages/CalculatorCircularBasket/CalculatorCircularBasket.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View, Text, ScrollView, SafeAreaView, TextInput } from 'react-native'
+import NumberInput from '../../components/NumberInput/NumberInput'
 
 import styles from './styles'
 
 const CalculatorCircularBasket = () => {
     return (
         <SafeAreaView>
-            <TextInput style={styles.textInput} placeholder="Input"></TextInput>
+            <NumberInput inputName="Diâmetro" />
             <Text>CalculatorCircularBasket</Text>
         </SafeAreaView>
     )
