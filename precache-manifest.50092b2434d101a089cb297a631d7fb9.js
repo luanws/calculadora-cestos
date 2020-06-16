26ef5cada43f3a153c8734d0a1b28c2cbf14dd8c
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-16 01:57:13 -0300

    .

---
+++ precache-manifest.50092b2434d101a089cb297a631d7fb9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1205158c88333ec5001",
+    "url": "/calculadora-cestos/static/js/app.e77ab5d8.chunk.js"
+  },
+  {
+    "revision": "1776e1920a3bd9443b34",
+    "url": "/calculadora-cestos/static/js/runtime~app.5d68ffa3.js"
+  },
+  {
+    "revision": "8f4c1178b5b606572f01",
+    "url": "/calculadora-cestos/static/js/2.9bc784d7.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/calculadora-cestos/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/calculadora-cestos/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/calculadora-cestos/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/calculadora-cestos/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/calculadora-cestos/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/calculadora-cestos/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/calculadora-cestos/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/calculadora-cestos/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/calculadora-cestos/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/calculadora-cestos/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/calculadora-cestos/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/calculadora-cestos/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/calculadora-cestos/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/calculadora-cestos/expo-service-worker.js"
+  },
+  {
+    "revision": "e741b1d32cdd6a0618e575edf6cadacb",
+    "url": "/calculadora-cestos/favicon-16.png"
+  },
+  {
+    "revision": "32aedab4726dd6d9153c40f1753ab29d",
+    "url": "/calculadora-cestos/favicon-32.png"
+  },
+  {
+    "revision": "d27e38031f4c44299b26a4c2be882023",
+    "url": "/calculadora-cestos/favicon.ico"
+  },
+  {
+    "revision": "aa2edcc55949e1d10a8910128271ddf7",
+    "url": "/calculadora-cestos/index.html"
+  },
+  {
+    "revision": "0df1db2a576926a5656c2dbc7d93d8ae",
+    "url": "/calculadora-cestos/manifest.json"
+  },
+  {
+    "revision": "b35cb15f807263ee0cd66dfa133eef59",
+    "url": "/calculadora-cestos/register-service-worker.js"
+  }
+];
